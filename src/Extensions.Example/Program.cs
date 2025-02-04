37befbae5290e125dfac91fe32cc41cbaf01fd4f
Jim Bennett <jim@pieces.app>
2025-02-04 13:02:59 -0800

    Reverting uncommented example

--- src/Extensions.Example/Program.cs
+++ src/Extensions.Example/Program.cs
@@ -408,26 +408,26 @@ var assets = await client.GetAssetsAsync().ConfigureAwait(false);
 
 // This example sets the system prompt
 
-{
-    // Create a Chat completion
-    var llamaModel = await client.DownloadModelAsync("llama-3 8B").ConfigureAwait(false);
-    IChatClient chatClient = new PiecesChatClient(client, chatName: $"Chat with a system prompt - {DateTime.Now.ToShortTimeString()}", logger: logger, llamaModel);
+// {
+//     // Create a Chat completion
+//     var llamaModel = await client.DownloadModelAsync("llama-3 8B").ConfigureAwait(false);
+//     IChatClient chatClient = new PiecesChatClient(client, chatName: $"Chat with a system prompt - {DateTime.Now.ToShortTimeString()}", logger: logger, llamaModel);
 
-    // Set the system prompt
-    var chatMessages = new List<ChatMessage>{
-        new(ChatRole.System, "Answer in the style of Yoda from Star Wars"),
-        new(ChatRole.User, "Hello")
-    };
+//     // Set the system prompt
+//     var chatMessages = new List<ChatMessage>{
+//         new(ChatRole.System, "Answer in the style of Yoda from Star Wars"),
+//         new(ChatRole.User, "Hello")
+//     };
 
-    await foreach (var r in chatClient.CompleteStreamingAsync(chatMessages).ConfigureAwait(false))
-    {
-        if (r.FinishReason != ChatFinishReason.Stop)
-        {
-            Console.Write(r.Text);
-        }
-    }
+//     await foreach (var r in chatClient.CompleteStreamingAsync(chatMessages).ConfigureAwait(false))
+//     {
+//         if (r.FinishReason != ChatFinishReason.Stop)
+//         {
+//             Console.Write(r.Text);
+//         }
+//     }
 
-    Console.WriteLine();
-}
+//     Console.WriteLine();
+// }
 
 #endregion Set the system prompt
